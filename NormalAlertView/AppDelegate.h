5fec90ffe6d181c21039230a4f72608efa2020a0
CheATSlark <jruijqx@163.com>
2017-11-27 20:59:27 +0800

    Initial Commit

---
+++ NormalAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NormalAlertView
+//
+//  Created by 焦瑞洁 on 2017/11/27.
+//  Copyright © 2017年 焦瑞洁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NormalAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NormalAlertView
+//
+//  Created by 焦瑞洁 on 2017/11/27.
+//  Copyright © 2017年 焦瑞洁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
